48ce68ca448f0ed78225fbac9b17a384c1bebba2
Mordy <64876593+kakapo-hdl@users.noreply.github.com>
2022-11-15 18:01:03 +0800

    test

---
+++ src/test/display.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+const Link: React.FC<{ page: string; children: any }> = ({ page, children }) => (
+  <a className="normal" href={page}>
+    {children}
+  </a>
+);
+
+it("renders correctly", () => {
+  const tree = renderer.create(<Link page="s">Facebook</Link>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
